c8d05d0334d1e77542feed9501eb095715fcb713
sharma.hemant <sharma.hemant@MDL-174-1QCL9X2.magicsw.com>
2021-12-01 13:47:31 +0530

    First commit

---
+++ Stars/src/Programming/Starpattern.java
@@ -0,0 +1,13 @@
+package Programming;
+
+public class Starpattern {
+
+	public static void main(String[] args) {
+		
+		for ( int i=0;i<=5;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println("*");
+	}}
+}
